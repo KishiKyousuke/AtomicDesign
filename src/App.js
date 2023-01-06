d0155f0454d807778653d9f28c688d416c8f1717
KishiKyousuke（Kyo18） <59789739+KishiKyousuke@users.noreply.github.com>
2023-01-07 00:37:09 +0900

    lec.32.33 organism

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organism/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "Kyo",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "hogehoge@example.com",
+  phone: "012-0987-0987",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
