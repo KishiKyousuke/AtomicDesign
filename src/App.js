60b23c0e0673bafab66a42a233b7322c039b09b3
KishiKyousuke（Kyo18） <59789739+KishiKyousuke@users.noreply.github.com>
2023-01-09 19:36:00 +0900

    lec.35 page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,6 @@
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organism/user/UserCard";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-import { BrowserRouter } from "react-router-dom";
+import { Router } from "./components/router/Router";
 import "./styles.css";
 
-const user = {
-  name: "Kyo",
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  email: "hogehoge@example.com",
-  phone: "012-0987-0987",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout className="App">
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
